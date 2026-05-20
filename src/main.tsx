0d42ebae0695ba8d4614212d3514bdd9f1248334
ZoeMelisko <48931999+ZoeMelisko@users.noreply.github.com>
2026-05-20 10:58:12 -0700

    Refactored ReactRouter from declaritive to data

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,29 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import {BrowserRouter, Routes, Route, useLocation } from "react-router";
+import {createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import ClassPage from "./pages/ClassPage.tsx";
 import ProfilePage from "./pages/ProfilePage.tsx";
-import {useLayoutEffect} from "react";
-
-const Wrapper = ({ children }) => {
-    const location = useLocation();
-
-    useLayoutEffect(() => {
-        // Scroll to the top of the page when the route changes
-        window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
-    }, [location.pathname]);
-
-    return children;
-};
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
 
 
-    <BrowserRouter>
-        <Wrapper>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/class/:c" element={<ClassPage />} />
-                <Route path="/class/:c/profile/:a" Component={(props)=><ProfilePage {...props} />} />
-                {/*<Route path="/${semester.semester}/${semester.name}" element={<AvatarProfilePage />} />*/}
-            </Routes>
-        </Wrapper>
-    </BrowserRouter>
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/class/:c",
+        element: <ClassPage />,
+    },
+    {
+        path: "/class/:c/profile/:a",
+        element: <ProfilePage />,
+    },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+    <RouterProvider router={router} />,
 );
